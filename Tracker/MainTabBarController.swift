590a243586ae29e0991209e0a138380b8ca74b90
Aleksei Tinkov <boofle@outlook.com>
2023-07-31 15:00:46 +0200

    создан пустой проект без storyboard

---
+++ Tracker/MainTabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  MainTabBarController.swift
+//  Tracker
+//
+//  Created by Алексей Тиньков on 31.07.2023.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+
+}
